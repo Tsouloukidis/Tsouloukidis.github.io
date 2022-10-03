714f71b1cbb7d45fc9d69e80604e4c7aba2213fc
Tsouloukidis <z-isis@windowslive.com>
2022-10-03 11:58:38 +0300

    portfolio website

---
+++ cv/index.js
@@ -0,0 +1,8 @@
+const menubtn = document.querySelector(".menubtn");
+const navbar = document.querySelector(".navbar");
+
+
+menubtn.addEventListener("click",function(){
+    navbar.classList.toggle("change");
+})
+
